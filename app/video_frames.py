dca077d46274adb917c6e09900d470103cf21afb
DrillPappa <fredrik@dorup.se>
2026-01-08 17:06:22 +0100

    Create video_frames.py

---
+++ app/video_frames.py
@@ -0,0 +1,7 @@
+# MVP-placeholder.
+# I denna första moln-version skickar vi hela videon till modellen och
+# använder ingen frame-extraktion. När allt fungerar kan vi uppgradera
+# med ffmpeg-baserad frame-sampling.
+
+def extract_frames(*args, **kwargs):
+    return []
